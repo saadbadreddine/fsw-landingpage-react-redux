e1fb3ac3abf1481475085995e61684f66a82f36d
Saad <badreddinesaad91@gmail.com>
2022-02-28 15:56:58 +0200

    added Routes to pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <h1>Test</h1>
+      <NavBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
     </>
   );
 }
