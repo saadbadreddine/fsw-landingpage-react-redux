ff28e8e99260f08a1b54baa1802d6baf24afba7a
Saad <badreddinesaad91@gmail.com>
2022-03-03 06:07:24 +0200

    added msgReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import msgReducer from "../features/auth/msgSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    msg: msgReducer,
   },
 });
